79b747ef8794b13dd5671da9ac7e0e846673e538
Your Name <you@example.com>
2021-01-26 00:16:44 +0300

    Add basic functions

--- app.py
+++ app.py
@@ -3,17 +3,33 @@ from flask import Flask, redirect
 from flask import request, jsonify, abort
 import json
 import logging
+import db
+import logging
+
+DB = db.DB()
 
 app = Flask(__name__)
+log = logging.getLogger(__name__)
+log.setLevel(logging.DEBUG)
+
+
+@app.route("/add")
+def add_new_short_link():
+    """"""
+    data = request.args.to_dict()
+    redirect_to = data["to"]
+    link = DB.add_new_short_link(redirect_to=redirect_to, )
+    return link
 
 
 @app.route("/<path:path>")
-def default(path):
-
-    return redirect("http://www." + path, code=302)
+def use_short(path):
+    link = DB.get_redirection_link(short_link=path)
+    log.debug(path)
+    return redirect("http://www." + link, code=302)
 
 
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run()
--- db.py
+++ db.py
@@ -3,7 +3,6 @@ from pymysql.cursors import DictCursor
 import config
 import logging
 import time
-from base64 import encodestring
 import Core
 
 log = logging.getLogger("db")
@@ -28,20 +27,21 @@ class DB:
     def add_new_short_link(self, redirect_to, user_id=0):
         """Add new shor link"""
         link = Core.generate_rand_hash()
-        cur = self.__cursor()
+        cur = self.connection.cursor()
         q = f"""INSERT into 
                 redirect_map(user_id, redirect_to , link)
-                VALUES
-                {user_id}, {redirect_to}, {link}
+                VALUES ({user_id}, '{redirect_to}', '{link}')
                 """
         cur.execute(q)
+        self.connection.commit()
         return link
 
     def get_redirection_link(self, short_link: str):
         cur = self.connection.cursor()
         q = f"""SELECT * from redirect_map
-                WHERE short_link = {short_link}
+                WHERE link = '{short_link}'
                 """
+        log.debug(q)
         cur.execute(q)
         try:
             return cur.fetchone()["redirect_to"]
