5dcd78ddefefa4298f28356af5ed3cccec8e14b6
Your Name <you@example.com>
2021-01-26 12:29:32 +0300

    Add urljoin

--- app.py
+++ app.py
@@ -5,12 +5,16 @@ import json
 import logging
 import db
 import logging
+from urllib.parse import urljoin
 
 DB = db.DB()
 
 app = Flask(__name__)
+app.config['DEBUG'] = True
+app.config['TESTING'] = True
+
 log = logging.getLogger(__name__)
-log.setLevel(logging.DEBUG)
+logging.basicConfig(level=logging.DEBUG)
 host = "127.0.0.1:5000/"
 
 
@@ -19,21 +23,24 @@ def add_new_short_link():
     """"""
     data = request.args.to_dict()
     redirect_to = data["to"]
-    link = DB.add_new_short_link(redirect_to=redirect_to, )
+    link = "s/" + DB.add_new_short_link(redirect_to=redirect_to)
     return f"""You'r link is <a href = "{link}">{host + link}</a>"""
 
 
-@app.route("/<path:path>")
+@app.route("/s/<path:path>")
 def use_short(path):
     link = DB.get_redirection_link(short_link=path)
-    log.debug(path)
-    return redirect("http://www." + link, code=302)
+    log.debug("Path: " + path)
+    log.debug("Link: " + link)
+    link_to_redirect = urljoin("https:", link)
+    log.debug(link_to_redirect)
+    return redirect(link_to_redirect, code=302)
 
 
 @app.route("/")
 def main():
     return """
-            <input></input><button>Add</button>
+            <input></input><button>Shorten</button>
             <script>
                 const button = document.querySelector('button');
                 const input = document.querySelector('input');
@@ -51,4 +58,4 @@ def main():
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
     port = int(os.environ.get('PORT', 5000))
-    app.run()
+    app.run(debug=True)
